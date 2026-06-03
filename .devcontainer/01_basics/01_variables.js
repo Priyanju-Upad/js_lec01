1a47687bcf902830da248863a6874c1765e8de7d
Priyanju Upadhyay <priyanjuupadhyay@gmail.com>
2026-06-03 04:08:32 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 145534;
+let accountEmail = "priyanjuupadhyay@gmail.com";
+var accountpwd = "12345"
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2;
+
+accountEmail="khushui.com";
+accountpwd="2121";
+accountCity="bengaluru"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountpwd,accountCity,accountState]);
+ /*
+ prefer to not use var 
+ because of issue in block scope and fxn scope
+ */
+
+
