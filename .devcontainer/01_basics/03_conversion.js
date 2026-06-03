12324ba3af1208aa11a11187dfba86d5cde7397a
Priyanju Upadhyay <priyanjuupadhyay@gmail.com>
2026-06-03 04:53:36 +0000

    config

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,26 @@
+let score ="33"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN  
+// true => 1 
+// false => 0     
+
+let isLoggedIn = 1
+
+let booleanIslogged = Boolean(isLoggedIn)
+console.log(booleanIslogged);
+
+// 1 => true
+// 0 => false
+//  "" => false
+//  "hitesh" =>true
+
+let someNo = 33
+let stringNumber = String(someNo)
+console.log(typeof stringNumber);
