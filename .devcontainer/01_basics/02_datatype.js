d67c67404836348d3431823628e25b1151550990
Priyanju Upadhyay <priyanjuupadhyay@gmail.com>
2026-06-03 04:27:16 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+// primitive data types---------------
+
+// number 
+//bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assign
+// symbol => to find uniqueness
+
+console.log(typeof "pihu");
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
+
