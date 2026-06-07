ed0c46098231a3dbe0cb28fac0e8495da7c9724e
Priyanju Upadhyay <priyanjuupadhyay@gmail.com>
2026-06-07 11:52:47 +0000

    change

---
+++ .devcontainer/03_js/03_loop.js
@@ -0,0 +1,14 @@
+// for loop
+for (let index = 0; index < 11; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+// while and do-while
+let i=0
+while (i<11) {
+ console.log(i);
+ i++;
+ 
+    
+}
