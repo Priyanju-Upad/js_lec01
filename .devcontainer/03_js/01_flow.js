cea7afaacad4b7802c40329de5cc58b3e3a7aa48
Priyanju Upadhyay <priyanjuupadhyay@gmail.com>
2026-06-07 10:35:03 +0000

    commit changes

---
+++ .devcontainer/03_js/01_flow.js
@@ -0,0 +1,31 @@
+// if
+
+if(2 === "2"){
+console.log("Executed");
+
+}
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+
+    case 2:
+        console.log("february");
+        break;
+
+    case 3:
+        console.log("March");
+        // break;
+
+    case 4:
+        console.log("april");
+         break;
+            
+
+    defaut:
+         break;
+        
+}
