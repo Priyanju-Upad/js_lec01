cadd6d984ffbae1ce7e6dc5949bba33de929b971
Priyanju Upadhyay <priyanjuupadhyay@gmail.com>
2026-06-08 14:18:56 +0000

    new

---
+++ .devcontainer/03_js/09_reduce.js
@@ -0,0 +1,15 @@
+// reduce 
+
+// accumulator is an empty variable 
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function (acc,currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval 
+// },3)
+
+const myTotal = myNums.reduce((acc,curr) => acc+curr,0)
+console.log(myTotal);
+
+// acc accumulator
+// currval current value  
